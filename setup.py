3502ef56f75df836a90969c26043b6562f41ddbd
y0081106 <30736790+y0081106@users.noreply.github.com>
2017-11-13 18:08:52 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+ 
+ 
+setup(name='fake_image',
+      version='0.1',
+      url='',
+      license='MIT',
+      author='Imaad',
+      author_email='imaadmkhan1@gmail.com',
+      description='Detect Fake Images',
+	  packages=setuptools.find_packages(),
+	  include_package_data=True)
