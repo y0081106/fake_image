945351563ae5260e55a389268be3efeb51e1c6e3
y0081106 <30736790+y0081106@users.noreply.github.com>
2017-12-04 23:41:56 +0100

    Update retraining.py

--- fake_image/retraining.py
+++ fake_image/retraining.py
@@ -32,8 +32,7 @@ def main(tweet):
 			self._return = None
 		def run(self):
 			if self._Thread__target is not None:
-				self._return = self._Thread__target(*self._Thread__args,
-													**self._Thread__kwargs)
+				self._return = self._Thread__target(*self._Thread__args,**self._Thread__kwargs)
 		def join(self):
 			Thread.join(self)
 			return self._return
